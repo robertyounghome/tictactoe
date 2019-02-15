322930290738a161a4759aed8bfbeea044dbf5a8
robertyounghome <robertyounghome@gmail.com>
2019-02-15 12:36:38 -0500

    Add files via upload

--- tktictactoe.py
+++ tktictactoe.py
@@ -1,3 +1,25 @@
+# Author: Bob Young
+# Original Date: 1/20/2019
+# Description: TicTacToe written in Python 3 with Tkinter GUI
+#
+# Thoughts: I read that TripleByte had asked some developers to write a TicTacToe program during interviews.
+# So I decided to give it a shot.  Originally, I decided to use the console for input from the user, 
+# and the computer would make moves randomly.  I had components of the program working within 30 minutes
+# or so, but did not have an full solution for over an hour.  Anyhow, after completing this, I decided 
+# to put a GUI front end on the program to make it more visually pleasing and easier to use.  
+# I used Tkinter for this.  At this point, I am somewhat intrigued by the prospect of
+# adding greater intelligence to the moves by the computer (AI of sorts).
+# After seeing what was typically done by other programmers who wrote TicTacToe programs, I see that a
+# simple list with indexes 0-8 is often used to keep track of the moves.  I opted to use a 3x3 matrix.
+# It was just the decision I made at the time.  Perhaps the simple list is somewhat easier, but really,
+# neither option offers much complexity.
+# Other possible changes for the future are allowing the user to go first or second (Be X's or O's).
+# Reporting stats on the GUI.  Note: I had stats (wins, losses, ties) reported to console originally.
+# The framework for keeping stats remains.
+# Also note however that if I decide to add AI to the computer moves, the computer should never lose 
+# and the reporting of stats gets a lot less interesting.
+
+
 import random
 from tkinter import *
 from tkinter import font
@@ -64,14 +86,17 @@ class Game:
 			return True
 		return False
 
+	# The game is over if somebody won or there is a tie
 	def over(self, player1, player2):
 		return self.won(player1, player2) or self.tied(player1, player2)
 
+	# Check our list of valid remaining moves to see if the move is valid
 	def checkMove(self, x, y):
 		if [x,y] in self.valid:
 			return True
 		return False
 
+    # Process the move by setting the board, the matrix and GUI, plus updating remaining valid moves
 	def move(self, x, y, s):
 		self.board[x][y] = s
 		print(self.buttons)
@@ -80,6 +105,8 @@ class Game:
 		self.turn += 1
 		print(self.board)
 
+	# Generates a random integer to determine the move by the computer
+	# This is the function that I would like to change to add intelligence to the computer moves.
 	def randomMove(self, s):
 		i = random.randint(0, len(self.valid) - 1)
 		t = self.valid[i]
@@ -87,46 +114,43 @@ class Game:
 		y = t[1]
 		self.move(x, y, s)
 
+	# Resets the GUI board
 	def boardReset(self):
 		for x in range(3):
 			for y in range(3):
 				game.buttons[x][y].configure(text='')
 
+    # Prompts user in the GUI.  Returns True if the user wants to play again, otherwise False
 	def playAgain(self, player = None):
 		if player == None:
 			return messagebox.askyesno("Game Over", f"Tie game!  Would you like to play again?")
 		else:
 			return messagebox.askyesno("Game Over", f"{player.name} wins!  Would you like to play again?")
 
+    # A move was made from our GUI
 	def tkmove(self, x, y, player, computer, root):
-		if (self.turn % 2 == 0 and player.text == 'X') or (self.turn % 2 and player.text == 'O'):
-			if self.checkMove(x, y):
-				self.move(x, y, player.text)
-				win = self.won(player, computer)
-				tie = self.tied(player, computer)
-				player1 = None
-				if win:
-					player1 = player
+		def winOrTie(player1, player2):
+			win = self.won(player1, player2)
+			tie = self.tied(player1, player2)
+			winner = None
+			if win:
+				winner = player
 				if win or tie:
-					if self.playAgain(player1):
+					if self.playAgain(winner):
 						self.reset()
 						self.boardReset()
 					else:
 						root.destroy()
-				else:
-					self.randomMove(computer.text)
-					win = self.won(computer, player)
-					tie = self.tied(computer, player)
-					player1 = None
-					if win:
-						player1 = computer
-					if win or tie:
-						if self.playAgain(player1):
-							self.reset()
-							self.boardReset()
-						else:
-							root.destroy()
 
+		if (self.turn % 2 == 0 and player.text == 'X') or (self.turn % 2 and player.text == 'O'):
+			if self.checkMove(x, y):
+				self.move(x, y, player.text)
+				winOrTie(player, computer)
+			else:
+				self.randomMove(computer.text)
+				winOrTie(computer, player)
+
+# GUI interface, clicking a button triggers an event calling tkmove of the Game class
 if __name__ == '__main__':
 	player = Player("Player", "X")
 	computer = Player("Computer", "O")
@@ -141,5 +165,5 @@ if __name__ == '__main__':
 			game.buttons[x] += [Button(root, text="", width=30, height=10, borderwidth=5, font=helv36, command=partial(game.tkmove, x, y, player, computer, root))]
 			game.buttons[x][y].grid(row=x,column=y)
 	root.mainloop()
-	
+
 
